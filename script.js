47e63ef9248de48d9d377f64c7d29a3e83251adf
Rakesh Munukuntla <130857014+Rakeshmunukuntla@users.noreply.github.com>
2024-11-29 10:05:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ inputs.forEach((input, index) => {
   input.addEventListener("input", (e) => {
     const value = e.target.value;
     if (value.length === 1 && index < inputs.length - 1) {
-      inputs[index + 1].focused();
+      inputs[index + 1].focus();
     }
   });
 
   input.addEventListener("keydown", (e) => {
     if (e.key === "Backspace" && !input.value && index > 0) {
-      inputs[index - 1].focused();
+      inputs[index - 1].focus();
     }
   });
 });
